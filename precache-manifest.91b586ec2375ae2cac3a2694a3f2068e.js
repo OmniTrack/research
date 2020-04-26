40c231b297932c3eabcad777cadb29bded00313c
Young-Ho Kim <yghokim@umd.edu>
2020-04-26 17:45:56 -0400

    Updates

---
+++ precache-manifest.91b586ec2375ae2cac3a2694a3f2068e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480fcd0668020030eca080f0d8048852",
+    "url": "/research/index.html"
+  },
+  {
+    "revision": "28ced0f07755b69457ba",
+    "url": "/research/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0309bfed636bce3872e",
+    "url": "/research/static/js/2.3bf77827.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/research/static/js/2.3bf77827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ced0f07755b69457ba",
+    "url": "/research/static/js/main.67f0ced8.chunk.js"
+  },
+  {
+    "revision": "c6377285f948f2546bfb",
+    "url": "/research/static/js/runtime-main.36e5f3ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/research/static/media/logo.5d5d9eef.svg"
+  }
+]);
